4ab8dabbca9c066a21350e32985ad19cb4fa3248
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-16 10:19:47 +0100

    completed js

--- app/javascript/components/init_autocomplete.js
+++ app/javascript/components/init_autocomplete.js
@@ -5,31 +5,41 @@ const wikiUrl = 'https://en.wikipedia.org'
 const params = 'action=query&list=search&format=json&origin=*'
 
 const autoComplete = () => {
-  new Autocomplete('#autocomplete', {
+  new Promise((resolve, reject) => {
+    new Autocomplete('#autocomplete', {
 
-    search: input => {
-      const url = `${wikiUrl}/w/api.php?${params
-        }&srsearch=${encodeURI(input)}`
+      search: input => {
+        const url = `${wikiUrl}/w/api.php?${params
+          }&srsearch=${encodeURI(input)}`
 
-      return new Promise(resolve => {
-        if (input.length < 3) {
-          return resolve([])
-        }
+        return new Promise(resolve => {
+          if (input.length < 3) {
+            return resolve([])
+          }
 
-        fetch(url)
-          .then(response => response.json())
-          .then(data => {
-            resolve(data.query.search)
-          })
-      })
-    },
+          fetch(url)
+            .then(response => response.json())
+            .then(data => {
+              resolve(data.query.search)
+            })
+        })
+      },
 
 
-    getResultValue: result => result.title,
-    onSubmit: result => {
-      window.open(`${wikiUrl}/wiki/${encodeURI(result.title)
-        }`)
-    }
+      getResultValue: result => result.title,
+      onSubmit: result => {
+        console.log('lol')
+        window.open(`${wikiUrl}/wiki/${encodeURI(result.title)
+          }`)
+      },
+
+      onShow: event => {
+        console.log(event)
+      },
+
+    })
+  }).then(() => {
+    console.log('kl')
   })
 }
 
