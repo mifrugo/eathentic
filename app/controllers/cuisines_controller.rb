9bd23e4cde007eb57d5618f03cc45b684987766b
Michele Dorazio <michele.dorazio96@gmail.com>
2021-03-16 17:28:38 +0100

    basic back-end ok

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,19 +1,12 @@
 class CuisinesController < ApplicationController
-  before_action :set_restaurants, only: [:show]
   def index
     @cuisines = policy_scope(Cuisine).order(created_at: :desc)
   end
 
   def show
-    raise
     @cuisine = Cuisine.find(params[:id])
+    @restaurants = Restaurant.where(cuisine_id:  @cuisine.id)
     authorize @cuisine
-
   end
 
-  private
-
-  def set_restaurants
-   # @restaurants = Restaurant.where(cuisine_id: @cuisine.id)
-  end  
 end
