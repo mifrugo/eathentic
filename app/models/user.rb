9328e136a92629c637fa731da4671fcbb78b66f7
Michele Dorazio <michele.dorazio96@gmail.com>
2021-03-17 15:49:28 +0100

    registration ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_validation :foo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,6 +9,11 @@ class User < ApplicationRecord
   has_many :dishes
   has_many :ingredients
   has_many :views
+  belongs_to :cuisine
 
   has_one_attached :avatar
+
+  def foo
+    cuisine_id = 12
+  end  
 end
