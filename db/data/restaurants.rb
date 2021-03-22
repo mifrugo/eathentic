b4556ce8aec542421cf15b341948b7b5f94dedca
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-22 12:37:17 +0100

    fixed seeds

---
+++ db/data/restaurants.rb
@@ -0,0 +1,403 @@
+
+  RESTAURANTS = [
+      {name: "Seu Pizza Illuminati",
+      description: "Try our signature pizza made with the best ingredients",
+      latitude: 41.8795008,
+      longitude: 12.4680942,
+      photo: "seupizza.jpeg",
+      dish: ["pizza margherita", "pizza with tuna", "pizza fior di zucca", "pizza diavola"]
+      },
+
+      {name: "Nannarella",
+      description: "Italian pasta",
+      latitude: 41.888943,
+      longitude: 12.470629,
+      photo: "nannarella.jpeg"
+      },
+
+      {name: "Tonnarello",
+      description: "Pasta homemade",
+      latitude: 41.889622,
+      longitude: 12.468848,
+      photo: "tonarello.jpeg"
+      },
+
+      {name: "Trattoria Monti",
+      description: "Italian cuisine",
+      latitude: 41.895805,
+      longitude: 12.501163,
+      photo: "monti.jpeg"
+      },
+
+      {name: "Armando al Pantheon",
+      description: "Traditional Italian cuisine",
+      latitude: 41.899067,
+      longitude: 12.47615051,
+      photo: "armando.jpeg"
+      },
+
+      {name: "ilSanlorenzo",
+      description: "Italian Fish Cuisine",
+      latitude: 41.895210,
+      longitude: 12.474231,
+      photo: "ilSanlorenzo.jpeg"
+      },
+
+      {name: "Flavio al Velavevodetto",
+      description: "Most creamy Carbonara",
+      latitude: 41.875015,
+      longitude: 12.475028,
+      photo: "velavevodetto.jpeg"
+      },
+
+      {name: "Da Remo",
+      description: "Roman Pizza",
+      latitude: 41.880154,
+      longitude: 12.475126,
+      photo: "remo.jpeg",
+      dish: ["pizza 4 cheese", "pizza napoli", "pizza fior di zucca", "pizza diavola"]
+      },
+
+      {name: "La Gatta Mangiona",
+      description: "One of the best roman Pizza",
+      latitude: 41.876804,
+      longitude: 12.449211,
+      photo: "gattamangiona.jpeg",
+      dish: ["pizza margherita", "pizza with tuna", "pizza salmon", "pizza diavola"]
+      },
+
+      {name: "Antico Arco",
+      description: "Special Caccio e Peppe with Fior di Zucca",
+      latitude: 41.888294,
+      longitude: 12.461287,
+      photo: "antico.jpeg",
+      dish: ["pizza margherita", "pizza salmon"]
+      },
+
+      {name: "Pizzarium",
+      description: "Special pizza to go - tomato and origano",
+      latitude: 41.906894,
+      longitude: 12.446501,
+      photo: "piazzarum.jpeg",
+      dish: ["pizza margherita", "pizza with tuna", "pizza salmon", "pizza diavola", "pizza capricciosa", "pizza boscaiola"]
+      },
+
+      {name: "La Pergola",
+      description: "The only 3 star Michelin restaurant in Rome - Carbonara special",
+      latitude: 41.919277,
+      longitude: 12.446052,
+      photo: "la-pergola.jpeg"
+      },
+
+      {name: "Maccheroni",
+      description: "The pasta home",
+      latitude: 41.900814,
+      longitude: 12.476006,
+      photo: "maccheroni.jpeg"
+      },
+
+      {name: "Osteria delle Coppelle",
+      description: "Deliscious pasta - with a touch of class",
+      latitude: 41.900814,
+      longitude: 12.476007,
+      photo: "copelle.jpeg"
+      },
+
+      {name: "Da Alfredo",
+      description: "The real Alfredo pasta",
+      latitude: 41.905842,
+      longitude: 12.477081,
+      photo: "alfredo-roma.jpeg"
+      },
+
+      {name: "180g Pizzeria Romana",
+      description: "Winner for the best roman pizza",
+      latitude: 41.878383,
+      longitude: 12.564086,
+      photo: "180gr.jpeg",
+      dish: ["pizza margherita", "pizza with tuna", "pizza salmon", "pizza diavola", "pizza napoli", "pizza boscaiola", "pizza capricciosa"]
+      },
+
+      {name: "Il Marchese",
+      description: "Traditional roman dishes for the real food lovers",
+      latitude: 41.906169,
+      longitude: 12.47567,
+      photo: "marchese.jpeg"
+      },
+
+      {name: "Da Enzo al 29",
+      description: "Serving Roman classics such as artichokes",
+      latitude: 41.888217,
+      longitude: 12.477868,
+      photo: "DaEnzo.jpeg"
+      },
+
+      {name: "Osteria Fratelli Mori",
+      description: "Family owned - focused in traditional roman plates",
+      latitude: 41.874469,
+      longitude: 12.479316,
+      photo: "moro.jpeg"
+      },
+
+      {name: "Roscioli salumeria con cucina",
+      description: "Bustling destination with an eatery serving Italian fare, plus a bakery, deli counter & wine shop",
+      latitude: 41.894264,
+      longitude: 12.474406,
+      photo: "Roscioli.jpeg"
+      },
+
+      {name: "Felice a Testaccio",
+      description: "Serving the best Caccio e Peppe - signature dish",
+      latitude: 41.87833,
+      longitude: 12.477421,
+      photo: "felice.jpeg"
+      },
+
+      {name: "L'Arcangelo",
+      description: "Serving the best Ammatriciana in Rome",
+      latitude: 41.907054,
+      longitude: 12.470369,
+      photo: "larcangelo.jpeg"
+      },
+
+      {name: "Da Cesare al Casaletto",
+      description: "The Real Gricia made with love",
+      latitude: 41.868041,
+      longitude: 12.438498,
+      photo: "cezare.jpeg"
+      },
+
+      {name: "Pinsere",
+      description: "Try some of the best pinsa in Rome",
+      latitude: 41.9076293,
+      longitude: 12.4977726,
+      photo: "pinsere.jpeg",
+      dish: ["pizza margherita", "pizza with tuna", "pizza salmon", "pizza diavola", "pizza funghi", "pizza boscaiola"]
+      },
+
+      {name: "Mizzica",
+      description: "Sicilian deliscious food - try our cannolo",
+      latitude: 41.911964,
+      longitude: 12.517305,
+      photo: "mizzica.jpeg"
+      },
+
+      {name: "Pompi",
+      description: "The King of Tiramissu in Rome.",
+      latitude: 41.880188,
+      longitude: 12.513369,
+      photo: "pompi.jpeg"
+      },
+
+      {name: "PummaRe Prati",
+      description: "The pizza home",
+      latitude: 41.909610,
+      longitude: 12.451728,
+      photo: "pummare.jpeg",
+      dish: ["pizza margherita", "pizza with tuna", "pizza salmon", "pizza diavola",  "pizza funghi"]
+      },
+
+      {name: "ArtePizza",
+      description: "Where art meets with pizza",
+      latitude: 41.909610,
+      longitude: 12.451728,
+      photo: "artepizza.jpeg",
+      dish: ["pizza salmon", "pizza diavola", "pizza fior di zucca", "pizza 4 cheese"]
+      },
+
+      {name: "Pizzeria Mimì Cocó",
+      description: "Food for people with taste",
+      latitude: 41.855693,
+      longitude: 12.480520,
+      photo: "mimicoco.jpeg",
+      dish: ["pizza margherita", "pizza diavola"]
+      },
+
+      {name: "Momart Restaurant Cafè",
+      description: "Try the best finger-food in Rome: The most rated aperitivo",
+      latitude: 41.919063,
+      longitude: 12.519779,
+      photo: "momart.jpeg",
+      dish: ["pizza with tuna", "pizza salmon", "pizza diavola"]
+      },
+       {name: "Siam Cuisine",
+      description: "Try our original dishes, you won't regret 😉",
+      latitude: 41.908727,
+      longitude: 12.491607,
+      photo: "siamc.jpg",
+      type: "thai",
+      dish: ["Grilled Chicken Asian Slaw", "Grilled Chicken Egg Noodle",
+             "Tom Yum Prawn Noodle", "Tom Yum Prawn Soup"]
+      },
+
+      {name: "Kisa Cuisine",
+      description: "If you're looking for a Thai restaurant in Rome there's nowhere else to go!",
+      latitude: 41.937918,
+      longitude: 12.5290832,
+      photo: "kisa.jpg",
+      type: "thai",
+      dish: ["Ka-Pow and Rice", "Green Curry and Rice", "Tom Yum Prawn Soup"]
+      },
+
+      {name: "Tora Sushi & Asian Cuisine",
+      description: "Discover Thai cuisine",
+      latitude: 41.8997,
+      longitude: 12.473897,
+      photo: "tora.jpg",
+      type: "thai",
+      dish: ["Roast Salmon Quinoa", "Sesame Chicken Salad",
+            "Somtam Salad Pot", "Massaman Curry and Rice"]
+      },
+
+      {name: "GO Thai",
+      description: "Come try our amazing dishes 🍜 ",
+      latitude: 41.870986,
+      longitude: 12.51806,
+      photo: "gothai.jpg",
+      type: "thai",
+      dish: ["Grilled Chicken Asian Slaw", "Pad Thai", "Somtam Salad Pot",
+            "Grilled Chicken Egg Noodle", "Roast Salmon Quinoa"]
+      },
+
+      {name: "CheKiang Royal Thai",
+      description: "Come find us in our elegant location",
+      latitude: 41.8845422,
+      longitude: 12.5223382,
+      photo: "chekiang.jpg",
+      type: "thai",
+      dish: ["Somtam Salad Pot", "Tom Yum Prawn Noodle", "Massaman Curry and Rice"]
+      },
+
+      {name: "Isola Puket",
+      description: "One of the best in Rome!",
+      latitude: 41.9343346,
+      longitude: 12.514232,
+      photo: "puket.jpg",
+      type: "thai",
+      dish: ["Ka-Pow and Rice", "Green Curry and Rice", "Roast Salmon Quinoa",
+             "Grilled Chicken Egg Noodle"]
+      },
+
+      {name: "Sukhothai",
+      description: "Where real Thai food is",
+      latitude: 41.8689654,
+      longitude: 12.4612272,
+      photo: "suko.jpg",
+      type: "thai",
+      dish: ["Fried rice with shrimps", "Vegetable spring rolls",
+             "Ka-Pow and Rice", "Green Curry and Rice"]
+      },
+
+      {name: "Tuk Tuk Ride",
+      description: "Best Pad Kra Prao in town",
+      latitude: 41.8593857,
+      longitude: 12.4814724,
+      photo: "tuk.jpg",
+      type: "thai",
+      dish: ["Grilled Chicken Egg Noodle", "Roast Salmon Quinoa",
+                "Somtam Salad Pot", "Vegetable spring rolls"]
+      },
+
+      {name: "Bali Bar",
+      description: "Thai restaurant in Trastevere",
+      latitude: 41.8904841,
+      longitude: 12.4673209,
+      photo: "balibar.jpg",
+      dish: ["Fried rice with shrimps", "Somtam Salad Pot", "Tom Yum Prawn Soup"]
+      },
+
+      {name: "Han Thai",
+      description: "Your local Thai restaurant 😋",
+      latitude: 41.7569845,
+      longitude: 12.3668536,
+      photo: "hanthai.jpg",
+      type: "thai",
+      dish: ["Grilled Chicken Asian Slaw", "Grilled Chicken Egg Noodle",
+            "Roast Salmon Quinoa", "Sesame Chicken Salad", "Somtam Salad Pot",
+            "Asian slaw", "Tom Yum Prawn Noodle", "Tom Yum Prawn Soup",
+            "Massaman Curry and Rice"]
+      },
+
+      {name: "L'Oasi del Gusto",
+      description: "Thai cuisine as we see it!",
+      latitude: 41.8917016,
+      longitude: 12.5617941,
+      photo: "oasi.jpg",
+      type: "thai",
+      dish: ["Green Curry and Rice", "Ka-Pow and Rice", "Fried rice with shrimps",
+             "Vegetable spring rolls"]
+      },
+
+      {name: "Taste East",
+      description: "You won't regret 😉",
+      latitude: 41.8732174,
+      longitude: 12.4408623,
+      photo: "tastethai.jpg",
+      type: "thai",
+      dish: ["Grilled Chicken Asian Slaw", "Grilled Chicken Egg Noodle",
+            "Fried rice with shrimps", "Vegetable spring rolls"]
+      },
+
+      {name: "Thai J",
+      description: "Discover authentc Thai cuisine",
+      latitude: 41.9118548,
+      longitude: 12.449925,
+      photo: "tjay.jpg",
+      type: "thai",
+      dish: ["Asian slaw", "Tom Yum Prawn Noodle", "Tom Yum Prawn Soup",
+            "Massaman Curry and Rice", "Fried rice with shrimps",
+            "Vegetable spring rolls"]
+      },
+
+      {name: "Ya Yuan",
+      description: "We are waiting for you!",
+      latitude: 41.980654,
+      longitude: 12.5080232,
+      photo: "yayuan.jpg",
+      type: "thai",
+      dish: ["Grilled Chicken Asian Slaw", "Grilled Chicken Egg Noodle",
+            "Somtam Salad Pot", "Asian slaw", "Ka-Pow and Rice"]
+      },
+
+      {name: "Thai Inn",
+      description: "Your new favorite Thai restaurant!",
+      latitude: 41.8792114,
+      longitude: 12.5082463,
+      photo: "inn.jpg",
+      type: "thai",
+      dish: ["Fried rice with shrimps", "Roast Salmon Quinoa", "Tom Yum Prawn Noodle", "Sesame Chicken Salad", "Green Curry and Rice"]
+      },
+
+      {name: "Kishi",
+      description: "Elegant location, fine cuisine",
+      latitude: 41.8961589,
+      longitude: 12.4307924,
+      photo: "kishi.jpg",
+      type: "thai",
+      dish: ["Grilled Chicken Asian Slaw", "Roast Salmon Quinoa",
+             "Massaman Curry and Rice", "Vegetable spring rolls", "Ka-Pow and Rice"]
+      },
+
+      {name: "Kung Fu",
+      description: "",
+      latitude: 45.6947423,
+      longitude: 9.0403104,
+      photo: "kungfu.jpg",
+      type: "thai",
+      dish: ["Sesame Chicken Salad", "Somtam Salad Pot", "Asian slaw",
+             "Fried rice with shrimps", "Vegetable spring rolls"]
+      },
+
+      {name: "Sandokan",
+      description: "Our amazing dishes will make you fall in love 💘",
+      latitude: 41.8601919,
+      longitude: 12.5577868,
+      type: "thai",
+      photo: "sandokan.jpg",
+      dish: ["Grilled Chicken Asian Slaw", "Grilled Chicken Egg Noodle",
+             "Roast Salmon Quinoa", "Sesame Chicken Salad", "Somtam Salad Pot",
+             "Asian slaw", "Tom Yum Prawn Noodle", "Tom Yum Prawn Soup",
+             "Massaman Curry and Rice", "Green Curry and Rice", "Ka-Pow and Rice",
+             "Fried rice with shrimps", "Vegetable spring rolls"]
+      }
+    ]
