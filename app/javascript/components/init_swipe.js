4988a1821cd45d5058007c87acaeceaee6bc771c
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-22 16:07:51 +0100

    fav

--- app/javascript/components/init_swipe.js
+++ app/javascript/components/init_swipe.js
@@ -1,11 +1,28 @@
+const vibrate = () => {
+  window.navigator.vibrate(50);
+}
+
 const cardSwipe = () => {
   document.querySelectorAll('.card').forEach(el => {
 
     let mc = new Hammer(el);
 
+    let Press = new Hammer.Press({
+      time: 500
+    });
+
     mc.on("swipeleft", function (ev) {
+      vibrate();
       el.click();
     });
+
+    mc.add(Press)
+
+    mc.on("press", function(e) {
+      e.preventDefault();
+      vibrate();
+      alert("Add to fav")
+    })
 
   })
 
@@ -15,8 +32,10 @@ const bodySwipe = () => {
   let mc = new Hammer(document.querySelector('body'))
 
   mc.on('swiperight', function() {
+    vibrate();
     document.getElementById('sidebarCollapse').click()
   })
 }
+
 
 export { cardSwipe, bodySwipe }
