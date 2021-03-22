48d2dd54903337193b8e45925167a707a3181643
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-22 17:38:13 +0100

    fix js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -9,27 +5,11 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-
-//
-// ----------------------------------------------------
-// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
-// WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
-
 // External imports
 import $ from 'jquery';
 window.$ = $ = jQuery;
 import "bootstrap";
 import "controllers";
-
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
 
 import { setVH } from '../components/init_vh';
 import { autoComplete } from '../components/init_autocomplete'
@@ -71,14 +51,16 @@ window.addEventListener('resize', () => {
 
 window.addEventListener('load', () => {
   navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    var serviceWorker;
-    if (registration.installing) {
-      serviceWorker = registration.installing;
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting;
-    } else if (registration.active) {
-      serviceWorker = registration.active;
-    }
-  }).catch(registrationError => {
-  });
+
+      let serviceWorker;
+      if (registration.installing) {
+        serviceWorker = registration.installing;
+      } else if (registration.waiting) {
+        serviceWorker = registration.waiting;
+      } else if (registration.active) {
+        serviceWorker = registration.active;
+      }
+    })
+
+  document.querySelector('#sidebar').classList.remove('d-none')
 });
