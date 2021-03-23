2cc11621f24479d11b610578944b6ad5773a08ba
Michele <53474707+mifrugo@users.noreply.github.com>
2021-03-24 00:19:52 +0100

    favorite js

---
+++ app/javascript/controllers/favorite_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from 'stimulus'
+
+const favorite = 'favorite'
+
+export default class extends Controller {
+
+
+  toggle(e) {
+    e.stopPropagation()
+    const card = this.element.closest('.card')
+
+    fetch(`/restaurants/${Number(card.dataset['restaurant'])}/favorite`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        authenticity_token: window._token
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if( data.action == 'removed') {
+          card.classList.remove(favorite)
+        } else {
+          card.classList.add(favorite)
+        }
+      })
+  }
+
+}
--- app/policies/favorite_restaurant_policy.rb
+++ app/policies/favorite_restaurant_policy.rb
@@ -2,4 +2,8 @@ class FavoriteRestaurantPolicy < ApplicationPolicy
   def create?
     true
   end
+
+  def favorite?
+    user
+  end
 end
