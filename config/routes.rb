364ae81cbc871a5fb72bbf496d783b122f8e4693
Besmir Gjata <besigjata@gmail.com>
2021-03-23 15:03:45 +0100

    created api for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get '/search', to: 'search#index'
+      post '/favorites', to: 'favorites#create'
     end
   end
 
