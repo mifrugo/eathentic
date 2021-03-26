5c6ac14bf1b2c76c9f2e329c7d79e03829a76f3e
Michele Frugoli <53474707+mifrugo@users.noreply.github.com>
2021-03-26 09:43:48 +0100

    Feat/vibrate (#144)
    * link transformation
    
    * reaction fixes

--- app/javascript/controllers/reaction_controller.js
+++ app/javascript/controllers/reaction_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "stimulus";
 export default class extends Controller {
 
   count() {
-
+    const classes = ['badge-info', 'badge-success', 'badge-warning']
     const aVote = this.element.getAttribute('data-value')
     const id = Number(this.element.closest("div").dataset["id"])
     fetch(`/reviews/${id}/reactions`, {
@@ -18,11 +18,17 @@ export default class extends Controller {
     })
       .then(response => response.json())
       .then(data => {
-        console.log(data.action)
-      if (data.action == "saved" ) {
-        document.querySelector(`.counter-${id} .badge`).innerHTML = data.counter
-        $(this.element).closest(".reactions-icons").removeClass('d-flex').slideUp()
-      }
+        if (data.action == "saved" ) {
+          let badge = document.querySelector(`.counter-${id} .badge`)
+          badge.innerHTML = data.counter
+          classes.forEach(c => badge.classList.remove(c))
+
+          badge.classList.add(
+            data.counter === 0 ? 'badge-warning' : ( data.counter > 0 ? 'badge-success' : 'badge-danger')
+          )
+
+          this.element.closest('.reaction-list').innerHTML = this.element.outerHTML
+        }
     })
   }
 }
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -5,7 +5,14 @@ class Reaction < ApplicationRecord
   validates :number, presence: true, inclusion: { in: [ -2, -1, 1, 2 ] }
   after_commit :compute_review
 
+  ICONS = {
+    '2': 'hearts.svg',
+    '-1': 'thumbdown.svg',
+    '1': 'thumbup.svg',
+    '-2': 'mad.svg'
+  }
+
   def compute_review
-    self.review.compute
+    review.compute
   end
 end
